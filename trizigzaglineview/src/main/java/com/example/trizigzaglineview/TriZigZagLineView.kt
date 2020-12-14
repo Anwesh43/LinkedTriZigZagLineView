ea83f3ecf90b1bab3a82b8c4099fca2975fa24cd
Anwesh43 <anweshthecool0@gmail.com>
2020-12-15 01:25:36 +0530

    adding Canvas extennsion methods

--- trizigzaglineview/src/main/java/com/example/trizigzaglineview/TriZigZagLineView.kt
+++ trizigzaglineview/src/main/java/com/example/trizigzaglineview/TriZigZagLineView.kt
@@ -29,3 +29,44 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawEndingLine(xSize : Float, ySize : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    drawLine(xSize * sc2, ySize * sc2, xSize * sc1, ySize * sc1, paint)
+}
+
+fun Canvas.drawTriZigZagLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val gap : Float = size / (2 * lineParts)
+    save()
+    translate(w / 2 - 2 * size, h / 2)
+    drawEndingLine(size, 0f, sc1.divideScale(0, parts), sc2.divideScale(0, parts), paint)
+    for (j in 0..(lineParts - 1)) {
+        val sck1 : Float = sc1.divideScale(1, parts).divideScale(j, lineParts)
+        val sck2 : Float = sc2.divideScale(1, parts).divideScale(j, lineParts)
+        save()
+        translate(size + j * 2 * gap, 0f)
+        drawEndingLine(gap, -gap, sck1.divideScale(0, triParts), sck2.divideScale(0, triParts), paint)
+        save()
+        translate(gap, -gap)
+        drawEndingLine(gap, gap, sck1.divideScale(1, triParts), sck2.divideScale(1, triParts), paint)
+        restore()
+        restore()
+    }
+    save()
+    translate(3 * size, 0f)
+    drawEndingLine(size, 0f, sc1.divideScale(2, parts), sc2.divideScale(2, parts), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawTZZLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriZigZagLine(scale, w, h, paint)
+}
