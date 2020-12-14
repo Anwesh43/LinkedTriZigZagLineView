2514dad585c091965d2532d2833874092e5fd096
Anwesh43 <anweshthecool0@gmail.com>
2020-12-15 01:27:10 +0530

    adding TriZigZagLineView class

--- trizigzaglineview/src/main/java/com/example/trizigzaglineview/TriZigZagLineView.kt
+++ trizigzaglineview/src/main/java/com/example/trizigzaglineview/TriZigZagLineView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawTZZLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriZigZagLine(scale, w, h, paint)
 }
+
+class TriZigZagLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
